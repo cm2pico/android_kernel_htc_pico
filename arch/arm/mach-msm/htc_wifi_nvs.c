33715d7150d627ca3d54eb8df163190176d82536
sakindia123 <sa6915@gmail.com>
2012-08-01 17:12:52 +0530

    htc: Misc cleanup

--- arch/arm/mach-msm/htc_wifi_nvs.c
+++ arch/arm/mach-msm/htc_wifi_nvs.c
@@ -37,7 +37,7 @@ static struct proc_dir_entry *wifi_data;
 
 unsigned char *get_wifi_nvs_ram( void )
 {
-	return wifi_nvs_ram;
+	return( wifi_nvs_ram );
 }
 EXPORT_SYMBOL(get_wifi_nvs_ram);
 
@@ -52,9 +52,8 @@ static int __init parse_tag_msm_wifi(const struct tag *tag)
 	size = min((tag->hdr.size - 2) * sizeof(__u32), NVS_MAX_SIZE);
 #ifdef ATAG_MSM_WIFI_DEBUG
 	printk("WiFi Data size = %d , 0x%x\n", tag->hdr.size, tag->hdr.tag);
-	for(i=0;( i < size );i++) {
+	for (i = 0; i < size; i++)
 		printk("%02x ", *dptr++);
-	}
 #endif	
 	memcpy(wifi_nvs_ram, dptr, size);
 	return 0;
